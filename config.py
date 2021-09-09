5cc7188f63185b0d38ce21c23288ac97662b05f4
JRM <jordeam@gmail.com>
2021-09-09 12:02:46 -0300

    Changing Flasky to Macbee

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class Config:
         ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'sel04012021101'
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'usx-sel0401cwj'
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Macbee]'
+    FLASKY_MAIL_SENDER = 'Macbee Admin <jordeam@gmail.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SSL_REDIRECT = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
